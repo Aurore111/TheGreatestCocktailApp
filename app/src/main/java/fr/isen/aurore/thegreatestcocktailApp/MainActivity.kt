8fd24aef8937f7b3b81202efd36f691c29f5a2d8
Aurore <aurore.jilliot@isen.yncrea.fr>
2026-02-16 11:06:48 +0100

    image

--- app/src/main/java/fr/isen/aurore/thegreatestcocktailApp/MainActivity.kt
+++ app/src/main/java/fr/isen/aurore/thegreatestcocktailApp/MainActivity.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.traceEventStart
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fr.isen.aurore.thegreatestcocktailApp.ui.theme.TheGreatestCocktailAppTheme
@@ -32,18 +36,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TheGreatestCocktailAppTheme {
-        Greeting("Android")
+    Button ("test")
+    {
+        Text(
+            text = "Hello $name!",
+        )
+        Text("hi")
     }
+
 }
-
-
