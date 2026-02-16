2fc8a48c28001ca21c496ded1f869116f7b4b544
Aurore <aurore.jilliot@isen.yncrea.fr>
2026-02-16 11:31:29 +0100

    image chocolat

--- app/src/main/java/fr/isen/aurore/thegreatestcocktailApp/MainActivity.kt
+++ app/src/main/java/fr/isen/aurore/thegreatestcocktailApp/MainActivity.kt
@@ -5,17 +5,21 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.traceEventStart
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.isen.aurore.thegreatestcocktailApp.ui.theme.TheGreatestCocktailAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,15 +40,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Button ({
-        Log.d("button", "click on button")
-    })
-    {
-        Text(
-            text = "Hello $name!",
+fun MonImage() {
+        Image(
+            painter = painterResource(id = R.drawable.chocolat),
+            contentDescription = "Description de l'image chocolat",
+            modifier = Modifier.size(200.dp)
         )
-        Text("hi")
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Button({
+            Log.d("button", "click on button")
+        })
+        {
+            Text(
+                text = "Hello $name!",
+            )
+            Text("hi")
+        }
+        Text("hiiiiii")
+        MonImage()
     }
 
 }
